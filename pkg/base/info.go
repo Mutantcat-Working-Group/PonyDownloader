d6b7650a72c5341f285676382821f4ad1b08e839
tyza66 <shun_@outlook.com>
2026-09-22 19:42:53 +0800

    ci: fix artifact flatten for AppImage upload, bump v1.0.20260925

--- pkg/base/info.go
+++ pkg/base/info.go
@@ -1,7 +1,7 @@
 package base
 
-// Version is the build version, set at build time, using `go build -ldflags "-X github.com/GopeedLab/gopeed/pkg/base.Version=1.0.20260924"`.
-var Version = "1.0.20260924"
+// Version is the build version, set at build time, using `go build -ldflags "-X github.com/GopeedLab/gopeed/pkg/base.Version=1.0.20260925"`.
+var Version = "1.0.20260925"
 var InDocker string
 
 func init() {
